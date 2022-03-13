17374750145ebc36c3da1267b7e407e5f6234e8c
Mustafe <mustafewaw@gmail.com>
2022-03-13 13:21:31 +0100

    basic php

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+// $fruit = "apple";
+
+// print($fruit);
+// $num3=4;
+// print($num3);
+
+// $a=3;
+// $b=2;
+// $sum_of_two_numbers = $a+$b;
+// print($sum_of_two_numbers);
+
+// echo and print
+// echo can take multiple parameters to output
+// echo is faster than print
+// echo 'hello','world';
+// print 'hello';
+
+// DATA TYPE
+// string: anything within quotes eg - 'hello world' "886"
+// integer: whole num: 0,1,-2,3
+// float: 1.22,10.5,12.0
+// boolean: true, False
+// echo 'hello', 'world';
+
+// if elseif else statement
+// if(condition){
+//   executes if condition is true
+// } else{
+//   executes if condition is false
+// }
+// Casharkan waa if elseif else statement
+// $a=5;
+// $b=10;
+// if($b<$a){
+//   echo 'b is greater than a';
+// } else{
+//   echo 'a is smaller than b';
+// }
+
+// $name='samiy';
+// if($name=='john'){
+//   echo 'john';
+// }elseif($name=='sami'){
+//   echo 'sami';
+// }else{
+//   echo 'wrong name';
+// }
+
+// Casharkani waa switch statement
+// switch (variable){
+//   case 'value':
+//     #code ...
+//     break;
+//     case 'value':
+//       #code ..
+//       break;
+//     default:
+//       # code ...
+//       break;
+// }
+
+// $num=12;
+// switch ($num){
+//   case 1:
+//     echo "the number is 1";
+//     break;
+    
+//     case 2:
+//       echo 'the number is 2';
+//       break;
+//     default:
+//       echo 'wrong number is choosen';
+// }
+
+// Casharkan waa While statement
+
+// while ( <= 10){
+//   # code...
+// }
+// $num = 1;
+// while ($num <= 10){
+//   echo 'The number is:$num<br>';
+//   // $num++;
+//   $num = $num+1;
+// }
+
+// Casharkan for loop
+// for statement
+// for ($i=0; $i < ; $i++){
+//   # code ...
+// }
+// for ($i=0; $i < 10; $i++){
+//   echo '$i<br>';
+// }
+// Casharka Array
+// $arr = ['apple','ball',3,23];
+// $arr = array['apple','ball',3]
+// $arr = ['apple','ball','cat'];
+// echo $arr[2];
+
+// $i=0;
+// $arr = ['apple','ball','cat'];
+// while($i<=2){
+//   echo $arr[$i],'<br>';
+//   $i++;
+// }
+
+// Casharka foreach loop
+
+
+
+
+?>
